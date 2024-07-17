64b998c5a012a35757560a7e31d5cf29f0d86af8
Richard Lyon <6917342+richardjlyon@users.noreply.github.com>
2024-07-17 14:11:06 -0400

    prepare for publishing

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -25,18 +25,17 @@ pub enum Commands {
 
 /// Creates a spinner.
 pub fn create_spinner(message: String) -> ProgressBar {
-    let new_spinner = ProgressBar::new_spinner();
-    let bar = new_spinner.with_message(message.clone());
+    let bar = ProgressBar::new_spinner().with_message(message);
     bar.enable_steady_tick(Duration::from_millis(100));
+
     bar
 }
 
 /// Creates a progress bar.
 pub fn create_progress_bar(size: u64, message: String) -> ProgressBar {
-    let progress_bar = ProgressBar::new(size).with_message(message).with_style(
+    ProgressBar::new(size).with_message(message).with_style(
         ProgressStyle::with_template("[{eta_precise}] {bar:40.cyan/blue} {msg}")
             .unwrap()
             .progress_chars("##-"),
-    );
-    progress_bar
+    )
 }
